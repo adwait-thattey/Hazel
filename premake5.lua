112a38295fac9ff661718abacd9fd6fcd247ff3b
Adwait Thattey <coderdude1999@gmail.com>
2022-07-20 23:51:10 +0530

    add sandbox project in lua

--- premake5.lua
+++ premake5.lua
@@ -74,6 +74,68 @@ project "Hazel"
 	
 
 
+project "Sandbox"
+	location "Sandbox" -- relative path from root
+	kind "ConsoleApp" -- for DLLs
+	language "C++"
+
+	targetdir	("bin/" .. outputdir .. "/%{prj.name}")
+	objdir	("bin-int/" .. outputdir .. "/%{prj.name}") -- intermediate folder
+
+	files
+	{
+		--[[
+		Compile All .h and .cpp files inside src dir
+		--]]
+
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp" 
+
+	}
+
+	include
+	{
+		"%{prj.name}/vendor/spdlog/include",
+		"Hazel/src"
+	}
+
+	links
+	{
+		"Hazel"
+	}
+
+
+	filter "system:windows"
+		-- everything under here only for windows
+
+		cppdialect "C++17"
+		staticruntime "On" -- linking runtime libraries statically
+		systemversion "latest" -- latest windows version
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
+
+
+		
+
+	-- these are all independant filters. To use multiple filters, specify multiple
+	filter "configurations:Debug"
+		defines	"HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configuration:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
+	-- multiple filters
+	filter {"system:windows", "configurations:Release"}
+		buildoptions "/MT"  -- enable multithread flag
 
 
 
